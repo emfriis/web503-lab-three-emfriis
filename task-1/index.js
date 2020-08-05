a8e6f4622cd01cfde26b0abf1fe645a37e3a2605
Emily Friis <emily-friis@live.nmit.ac.nz>
2020-08-06 10:58:24 +1200

    Fixed quad colour.

--- task-1/index.js
+++ task-1/index.js
@@ -4,7 +4,7 @@ function setup() {
 }
 
 function draw() {
-    fill(255, 204, 100); // Selects blue fill colour.
+    fill('blue'); // Selects blue fill colour.
     noStroke(); // Removes stroke from draw function.
     quad(38, 31, 86, 20, 69, 63, 30, 76); // Draws a quad with corners at specified coords.
     rotate(35); // Rotates the shape by 35 degrees.
